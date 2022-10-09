8451542125607a6cd982b2ac1eba44c66a385597
Nehemie <nehemie@opportuneur.co.uk>
2022-10-09 06:06:44 +0200

    #Chore: Practicing react

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <header className="absolute bg-slate-100 min-h-[60px] left-0 right-0 top-0"></header>
+  );
+}
---
+++ src/views/App.tsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import "../styles/index.css";
+
+function App() {
+  return (
+    <div className="bg-slate-50 min-h-screen">
+      <div className="mx-auto max-w-screen-sm relative shadow-lg min-h-screen max-h-screen">
+        {/* Header */}
+        <Header></Header>
+
+        {/* Body */}
+
+        {/* Footer */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
